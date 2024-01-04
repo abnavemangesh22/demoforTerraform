60e68d800e28a58bba22bc5cd70932088a960eec
Mangesh <mangesh@yahoo.com>
2024-01-04 16:12:04 +0530

    this is v3

--- provider.tf
+++ provider.tf
@@ -3,4 +3,7 @@ provider "azurerm" {
  }
  skip_provider_registration = true
  subscription_id = "741f607e-5ee2-420c-a1f4-3fc69ee59cba"
+ client_id = "c9b144f9-067e-45a8-81d8-cb7eb3963d9f"
+ tenant_id = "673473ea-81b5-4999-a292-6eba731abbfe"
+ client_secret = "89ec9556-16ce-4ea8-8335-4e70f5377232"
 }
